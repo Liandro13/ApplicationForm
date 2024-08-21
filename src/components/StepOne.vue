f3f70df4aaf35834a361298f3c4b82282ebd2ff4
Liandro13 <liandrodacruz@outlook.pt>
2024-08-21 13:46:36 +0100

    Mensagem descritiva sobre o commit

---
+++ src/components/StepOne.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="form-steps" :class="{ active: currentStep === 1 }" id="step1">
+    <h3>Passo 1: Dados Pessoais</h3>
+    <form @submit.prevent="handleSubmit" class="job-application-form" id="commentForm1">
+      <div class="form-group">
+        <input
+          type="text"
+          :value="name"
+          @input="$emit('update:name', $event.target.value)"
+          class="form-control"
+          id="input-name"
+          required
+        />
+        <label for="input-name">Nome Completo</label>
+      </div>
+      <div class="form-group">
+        <input
+          type="email"
+          :value="email"
+          @input="$emit('update:email', $event.target.value)"
+          class="form-control"
+          id="input-email"
+          required
+        />
+        <label for="input-email">Endereço de Email</label>
+      </div>
+      <button type="submit" class="btn step-btn">Próximo</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['currentStep', 'name', 'email'],
+  methods: {
+    handleSubmit() {
+      this.$emit('next', 1);
+    },
+  },
+};
+</script>
---
+++ src/components/StepThree.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="form-steps" :class="{ active: currentStep === 3 }" id="step3">
+    <a href="#" class="prev" @click.prevent="prevStep(3)"><i class="las la-arrow-left"></i>Passo Anterior</a>
+    <h3>Passo 3: Mensagem e Termos</h3>
+    <form @submit.prevent="handleSubmit" class="job-application-form" id="commentForm3">
+      <div class="form-group">
+        <textarea
+          class="form-control"
+          :value="message"
+          @input="$emit('update:message', $event.target.value)"
+          id="inputMessage"
+        ></textarea>
+        <label for="inputMessage">Mensagem Opcional</label>
+      </div>
+      <div class="form-group check-group">
+        <div class="custom-checkbox">
+          <input
+            type="checkbox"
+            :checked="termsAccepted"
+            @change="$emit('update:termsAccepted', $event.target.checked)"
+            class="custom-control-input"
+            id="terms"
+            required
+          />
+          <label class="custom-control-label" for="terms">Li e aceito os <a href="#">Termos e Condições</a></label>
+        </div>
+      </div>
+      <button type="submit" class="btn" id="submitForm">Submeter</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['currentStep', 'message', 'termsAccepted'],
+  methods: {
+    prevStep() {
+      this.$emit('prev', 3);
+    },
+    handleSubmit() {
+      this.$emit('submit');
+    },
+  },
+};
+</script>
