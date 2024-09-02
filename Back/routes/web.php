d3aca838a6d346a8525f88afbac1833621451204
Liandro13 <liandrodacruz@outlook.pt>
2024-09-02 11:38:49 +0100

    Routes / updated

--- Back/routes/web.php
+++ Back/routes/web.php
@@ -39,3 +39,11 @@ Route::middleware(['auth', 'admin'])->group(function () {
 use App\Http\Controllers\CandidaturaController;
 Route::apiResource('candidaturas', CandidaturaController::class);
 
+Route::get('/candidaturas', [CandidaturaController::class, 'index'])->name('candidaturas.index');
+Route::get('/candidaturas/create', [CandidaturaController::class, 'create'])->name('candidaturas.create');
+Route::post('/candidaturas', [CandidaturaController::class, 'store'])->name('candidaturas.store');
+Route::get('/candidaturas/{candidatura}', [CandidaturaController::class, 'show'])->name('candidaturas.show');
+Route::get('/candidaturas/{candidatura}/edit', [CandidaturaController::class, 'edit'])->name('candidaturas.edit');
+Route::put('/candidaturas/{candidatura}', [CandidaturaController::class, 'update'])->name('candidaturas.update');
+Route::delete('/candidaturas/{candidatura}', [CandidaturaController::class, 'destroy'])->name('candidaturas.destroy');
+
--- BackEnd/form/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-// routes/api.php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CandidaturaController;
-
-Route::apiResource('candidaturas', CandidaturaController::class);
-
-
